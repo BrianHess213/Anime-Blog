71a0c45f18ef529abcc5048eecee6d5d15a5248d
Brian Hess <brianhess213@gmail.com>
2023-04-14 23:51:11 -0600

    Added Wrong Files

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
@@ -8,24 +9,33 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
+}
+
+function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
     </ThemeProvider>
   )
 }
+const exportedObject = { MyApp, App }
+export default exportedObject
