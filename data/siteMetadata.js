551ae49e23c20571d2a2853352b58b0539c3b25e
Brian Hess <brianhess213@gmail.com>
2023-04-14 23:07:43 -0600

    Adding Vercel Analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,7 +10,7 @@ const siteMetadata = {
   siteLogo: '/static/images/AsherLogo.png',
   image: '/static/images/AsherLogo.png',
   socialBanner: '/static/images/twitter-card.png',
-  // email: 'TakayukiAsher@gmail.com',
+  //email: 'TakayukiAsher@gmail.com',
   // github: 'https://github.com',
   twitter: 'https://twitter.com/AsherTakayuki',
   // facebook: 'https://facebook.com',
